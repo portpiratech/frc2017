bacad12a670afdaab3d1a93e45ff1b5d2282f52f
Curtis Allen Gibeaut Jr <cgibeaut@insomdev.com>
2017-01-14 09:34:23 -0600

    Removing unused files

--- Dirigible/src/org/usfirst/frc/team4804/robot/Robot.java
+++ Dirigible/src/org/usfirst/frc/team4804/robot/Robot.java
@@ -1,9 +1,7 @@
 
 package org.usfirst.frc.team4804.robot;
 
-import org.usfirst.frc.team4804.robot.commands.ExampleCommand;
 import org.usfirst.frc.team4804.robot.subsystems.DriveTrain;
-import org.usfirst.frc.team4804.robot.subsystems.ExampleSubsystem;
 import org.usfirst.frc.team4804.robot.subsystems.Shooter;
 
 import edu.wpi.first.wpilibj.IterativeRobot;
@@ -22,7 +20,6 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
  */
 public class Robot extends IterativeRobot {
 
-	public static final ExampleSubsystem exampleSubsystem = new ExampleSubsystem();
 	public static final DriveTrain driveTrain = new DriveTrain(RobotMap.TANKDRIVE_LEFT_ID, RobotMap.TANKDRIVE_RIGHT_ID);
 	public static final Shooter shooter = new Shooter(RobotMap.SHOOTINGDRIVE_ID);
 	public static OI oi;
@@ -37,7 +34,7 @@ public class Robot extends IterativeRobot {
     public void robotInit() {
 		oi = new OI();
         chooser = new SendableChooser();
-        chooser.addDefault("Default Auto", new ExampleCommand());
+        
 //        chooser.addObject("My Auto", new MyAutoCommand());
         SmartDashboard.putData("Auto mode", chooser);
     }
