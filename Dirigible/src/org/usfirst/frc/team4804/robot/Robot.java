622eca3c7ac4dea36d0494fac875c539eb4a3ffb
PortPiraTech1 <PortPiraTech1@PortPiraTech1pc>
2017-02-11 14:54:42 -0600

    Moved vision into subsystem. Not very functional

--- Dirigible/src/org/usfirst/frc/team4804/robot/Robot.java
+++ Dirigible/src/org/usfirst/frc/team4804/robot/Robot.java
@@ -53,25 +53,6 @@ public class Robot extends IterativeRobot {
      * used for any initialization code.
      */
     public void robotInit() {
-    
-    	new Thread(() -> {
-    		UsbCamera camera = CameraServer.getInstance().startAutomaticCapture();
-    		camera.setResolution(640, 480);
-    		
-    		CvSink  cvSink = CameraServer.getInstance().getVideo();
-    		CvSource outputStream = CameraServer.getInstance().putVideo("Blur", 640, 480);
-    	
-    		Mat source = new Mat();
-    		Mat output = new Mat();
-    		
-    		while(!Thread.interrupted()){
-    			cvSink.grabFrame(source);
-    			Imgproc.cvtColor(source, output, Imgproc.COLOR_BGR2GRAY);
-    			outputStream.putFrame(output);
-    		}
-    		
-    	}).start();
-    	
     	
 		oi = new OI();
         chooser = new SendableChooser();
--- Dirigible/src/org/usfirst/frc/team4804/robot/subsystems/VisionSubsystem.java
+++ Dirigible/src/org/usfirst/frc/team4804/robot/subsystems/VisionSubsystem.java
@@ -1,8 +1,14 @@
 package org.usfirst.frc.team4804.robot.subsystems;
 
-import org.usfirst.frc.team4804.robot.Robot;
+import org.opencv.core.Core;
+import org.opencv.core.Mat;
+import org.opencv.imgproc.Imgproc;
 import org.usfirst.frc.team4804.robot.commands.VisionDisplay;
 
+import edu.wpi.cscore.CvSink;
+import edu.wpi.cscore.CvSource;
+import edu.wpi.cscore.UsbCamera;
+import edu.wpi.first.wpilibj.CameraServer;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
 /**
@@ -44,8 +50,22 @@ public class VisionSubsystem extends Subsystem {
 		//}
 	}
 	
-	public void frameAutoDisplay() {
+	public void cameraInit() {
+		UsbCamera camera = CameraServer.getInstance().startAutomaticCapture();
+		camera.setResolution(640, 480);
+	}
+	
+	public void frameAutoDisplay() {		
+		CvSink  cvSink = CameraServer.getInstance().getVideo();
+		CvSource outputStream = CameraServer.getInstance().putVideo("Blur", 640, 480);
+	
+		Mat source = new Mat();
+		Mat output = new Mat();
 		
+		cvSink.grabFrame(source);
+		Core.flip(source, source, -1);
+		Imgproc.cvtColor(source, output, Imgproc.COLOR_BGR2GRAY);
+		outputStream.putFrame(output);
 	}
 	
 	public void enableProcessing() {
